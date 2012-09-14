4cdb8550540772de7773a5361fd97ce8e99b4c52
Takuya Akiba <iw@iwi.tc>
2012-09-14 10:06:46 +0900

    SRM 555

---
+++ topcoder/srm555/XorBoardDivTwo.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <cassert>
+#include <climits>
+#include <numeric>
+using namespace std;
+
+#define all(c) (c).begin(), (c).end()
+#define iter(c) __typeof((c).begin())
+#define cpresent(c, e) (find(all(c), (e)) != (c).end())
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define tr(c, i) for (iter(c) i = (c).begin(); i != (c).end(); ++i)
+#define pb push_back
+#define mp make_pair
+
+struct XorBoardDivTwo {
+  int theMax(vector <string> board);
+};
+
+
+
+int XorBoardDivTwo::theMax(vector <string> board) {
+  int H = board.size();
+  int W = board[0].length();
+
+  int ans = 0;
+  rep (fy, H) rep (fx, W) {
+    int tmp = 0;
+    rep (y, H) rep (x, W) tmp += (board[y][x] - '0') ^ (y == fy) ^ (x == fx);
+    ans = max(ans, tmp);
+  }
+
+  return ans;
+}
+
+
+
+
+
+
+
+
+
+
+
+// BEGIN CUT HERE
+namespace moj_harness {
+	int run_test_case(int);
+	void run_test(int casenum = -1, bool quiet = false) {
+		if (casenum != -1) {
+			if (run_test_case(casenum) == -1 && !quiet) {
+				cerr << "Illegal input! Test case " << casenum << " does not exist." << endl;
+			}
+			return;
+		}
+
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = run_test_case(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+
+		if (total == 0) {
+			cerr << "No test cases run." << endl;
+		} else if (correct < total) {
+			cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << endl;
+		} else {
+			cerr << "All " << total << " tests passed!" << endl;
+		}
+	}
+
+	int verify_case(int casenum, const int &expected, const int &received, clock_t elapsed) {
+		cerr << "Example " << casenum << "... ";
+
+		string verdict;
+		vector<string> info;
+		char buf[100];
+
+		if (elapsed > CLOCKS_PER_SEC / 200) {
+			sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+			info.push_back(buf);
+		}
+
+		if (expected == received) {
+			verdict = "PASSED";
+		} else {
+			verdict = "FAILED";
+		}
+
+		cerr << verdict;
+		if (!info.empty()) {
+			cerr << " (";
+			for (int i=0; i<(int)info.size(); ++i) {
+				if (i > 0) cerr << ", ";
+				cerr << info[i];
+			}
+			cerr << ")";
+		}
+		cerr << endl;
+
+		if (verdict == "FAILED") {
+			cerr << "    Expected: " << expected << endl;
+			cerr << "    Received: " << received << endl;
+		}
+
+		return verdict == "PASSED";
+	}
+
+	int run_test_case(int casenum__) {
+		switch (casenum__) {
+		case 0: {
+			string board[]            = {"101",
+ "010",
+ "101"};
+			int expected__            = 9;
+
+			clock_t start__           = clock();
+			int received__            = XorBoardDivTwo().theMax(vector <string>(board, board + (sizeof board / sizeof board[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 1: {
+			string board[]            = {"111",
+ "111",
+ "111"};
+			int expected__            = 5;
+
+			clock_t start__           = clock();
+			int received__            = XorBoardDivTwo().theMax(vector <string>(board, board + (sizeof board / sizeof board[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 2: {
+			string board[]            = {"0101001",
+ "1101011"};
+			int expected__            = 9;
+
+			clock_t start__           = clock();
+			int received__            = XorBoardDivTwo().theMax(vector <string>(board, board + (sizeof board / sizeof board[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 3: {
+			string board[]            = {"000",
+ "001",
+ "010",
+ "011",
+ "100",
+ "101",
+ "110",
+ "111"};
+			int expected__            = 15;
+
+			clock_t start__           = clock();
+			int received__            = XorBoardDivTwo().theMax(vector <string>(board, board + (sizeof board / sizeof board[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 4: {
+			string board[]            = {"000000000000000000000000",
+ "011111100111111001111110",
+ "010000000100000001000000",
+ "010000000100000001000000",
+ "010000000100000001000000",
+ "011111100111111001111110",
+ "000000100000001000000010",
+ "000000100000001000000010",
+ "000000100000001000000010",
+ "011111100111111001111110",
+ "000000000000000000000000"}
+;
+			int expected__            = 105;
+
+			clock_t start__           = clock();
+			int received__            = XorBoardDivTwo().theMax(vector <string>(board, board + (sizeof board / sizeof board[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+
+		// custom cases
+
+/*      case 5: {
+			string board[]            = ;
+			int expected__            = ;
+
+			clock_t start__           = clock();
+			int received__            = XorBoardDivTwo().theMax(vector <string>(board, board + (sizeof board / sizeof board[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 6: {
+			string board[]            = ;
+			int expected__            = ;
+
+			clock_t start__           = clock();
+			int received__            = XorBoardDivTwo().theMax(vector <string>(board, board + (sizeof board / sizeof board[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 7: {
+			string board[]            = ;
+			int expected__            = ;
+
+			clock_t start__           = clock();
+			int received__            = XorBoardDivTwo().theMax(vector <string>(board, board + (sizeof board / sizeof board[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+
+
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		moj_harness::run_test();
+	} else {
+		for (int i=1; i<argc; ++i)
+			moj_harness::run_test(atoi(argv[i]));
+	}
+}
+// END CUT HERE
