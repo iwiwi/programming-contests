3fccf21b8c174272af35072bcb58bef09ac9bd36
iwiwi <t.akiba.65536@gmail.com>
2013-12-16 11:22:40 +0900

    Facebook Hacker Cup 2014 Round 2

---
+++ fhc/2014/r2/magic_pairs.cpp
@@ -0,0 +1,275 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <cassert>
+#include <cstdarg>
+#include <sys/time.h>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+using namespace std;
+
+#define BEGIN_SOLVER struct solver {
+#define END_SOLVER };
+
+const int MAX_OUT = 10000;
+const char *OUTPUT_FORMAT = "Case #%d: %s"; //"Case #%d:\n%s";
+
+#define all(c) (c).begin(), (c).end()
+#define iter(c) __typeof((c).begin())
+#define present(c, e) ((c).find((e)) != (c).end())
+#define cpresent(c, e) (find(all(c), (e)) != (c).end())
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define tr(c, i) for (iter(c) i = (c).begin(); i != (c).end(); ++i)
+#define pb push_back
+#define mp make_pair
+
+typedef long long ll;
+
+void init() {
+
+}
+
+
+
+BEGIN_SOLVER ///////////////////////////////////////////////////////////////////////////////////////
+
+int N, M;
+ll x1, x2, a1, a2, b1, b2, c1, c2, r1, r2;
+
+ll C[2][1000010];
+
+ll dif;
+map<ll, ll> mem[2];
+
+void input() {
+  cin >> N >> M;
+  cin >> x1 >> a1 >> b1 >> c1 >> r1;
+  cin >> x2 >> a2 >> b2 >> c2 >> r2;
+}
+
+void gen() {
+  C[0][0] = x1;
+  C[1][0] = x2;
+  for (ll i = 1; i < max(N, M); ++i) {
+    if (i < N) C[0][i] = (a1 * C[0][(i-1) % N] + b1 * C[1][(i-1) % M] + c1) % r1;
+    if (i < M) C[1][i] = (a2 * C[0][(i-1) % N] + b2 * C[1][(i-1) % M] + c2) % r2;
+  }
+}
+
+ll next_color(ll x, ll *cs, ll n, map<ll, ll> &me, map<ll, ll> &op) {
+  {
+    ll c = cs[x];
+    assert(me[c] == 0);
+    ++me[c];
+    if (op[c] == 0) ++dif;
+    else --dif;
+    ++x;
+  }
+
+  for (; x < n; ++x) {
+    ll c = cs[x];
+    if (me[c] == 0) break;
+  }
+  return x;
+}
+
+void solve() {
+  memset(C, -1, sizeof(C));
+  gen();
+  rep (i, N) assert(C[0][i] != -1);
+  rep (i, M) assert(C[1][i] != -1);
+
+  dif = 0;
+  ll x = 0, y = 0, ans = 0;
+  while (x < N && y < M) {
+    ll tx = next_color(x, C[0], N, mem[0], mem[1]);
+    ll ty = next_color(y, C[1], M, mem[1], mem[0]);
+    if (dif == 0) ans += (tx - x) * (ty - y);
+    x = tx;
+    y = ty;
+  }
+  printf("%lld\n", ans);
+}
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Template
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+char out_buf[MAX_OUT], *out_p;
+
+solver() : out_p(NULL) {}
+
+
+
+void printf(const char* format, ...)
+  __attribute__((format(printf, 2, 3)))
+{
+  if (!out_p) out_p = out_buf;
+
+  va_list args;
+  va_start(args, format);
+  out_p += vsnprintf(out_p, sizeof(char) * (MAX_OUT - (out_p - out_buf)), format, args);
+  va_end(args);
+
+  if (out_p - out_buf >= MAX_OUT) {
+    fprintf(stderr, "error: Output Limit Exceeded !!\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void puts(const char *s) {
+  printf("%s\n", s);
+}
+
+void debug(const char* format, ...)
+  __attribute__((format(printf, 2, 3)))
+{
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  fflush(stderr);
+}
+
+
+
+END_SOLVER /////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+double sec() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec + tv.tv_usec * 1e-6;
+}
+
+
+
+void print_status(int c, int C, double t0, double t1, int nth) {
+  static const int L = 5;
+  if (C > L && c % (C / L) != 0) return;
+  else if (c - (nth - 1) / 2 <= 0) fprintf(stderr, "[ case: %d / %d ]\n", c, C);
+  else {
+    double t = sec();
+    fprintf(stderr, "[ case: %d / %d | time: %.3f / %.3f ]\n",
+            c, C, t - t0, (t1 - t0) + (t - t1) / (c - (nth - 1) / 2) * C);
+  }
+}
+
+
+
+int main(int argc, char **argv) {
+  bool parallel = false, status = false;
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-p") == 0) parallel = status = true;
+    else if (strcmp(argv[i], "-s") == 0) status = true;
+    else {
+      fprintf(stderr, "usage: %s [-p] [-s]\n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  double t0 = sec();
+  init();
+  double t1 = sec();
+  if (status) fprintf(stderr, "[ init: %.3f ]\n", t1 - t0);
+
+  string tmp;
+  getline(cin, tmp);
+  int C = atoi(tmp.c_str());
+
+  if (!parallel) {
+    if (status) fprintf(stderr, "[ mode: single thread ]\n");
+
+    rep (c, C) {
+      if (status) print_status(c, C, t0, t1, 1);
+
+      solver *s = new solver();
+      assert(s != NULL);
+      s->input();
+      s->solve();
+      printf(OUTPUT_FORMAT, c + 1, s->out_buf);
+      fflush(stdout);
+      delete s;
+    }
+  }
+  else {
+#ifdef _OPENMP
+
+    int nth = omp_get_max_threads();
+    if (status) fprintf(stderr, "[ mode: parallel (%d) ]\n", nth);
+
+    vector<string> out(C);
+    vector<bool> done(C);
+    int solve_c = 0, out_c = 0;
+    omp_lock_t lock;
+    omp_init_lock(&lock);
+
+#pragma omp parallel for schedule(dynamic, 1)
+    rep (c, C) {
+      solver *s = new solver();
+      assert(s != NULL);
+
+      int my_c;
+      omp_set_lock(&lock);
+      {
+        while (out_c < C && done[out_c]) {
+          printf(OUTPUT_FORMAT, out_c + 1, out[out_c].c_str());
+          fflush(stdout);
+          out_c++;
+        }
+        if (status) print_status(solve_c, C, t0, t1, nth);
+        my_c = solve_c++;
+        s->input();
+      }
+      omp_unset_lock(&lock);
+
+      s->solve();
+
+      omp_set_lock(&lock);
+      {
+        out[my_c] = string(s->out_buf);
+        done[my_c] = true;
+      }
+      omp_unset_lock(&lock);
+      delete s;
+    }
+
+    omp_destroy_lock(&lock);
+
+    while (out_c < C) {
+      assert(done[out_c]);
+      printf(OUTPUT_FORMAT, out_c + 1, out[out_c].c_str());
+      out_c++;
+    }
+
+#else
+    fprintf(stderr, "error: compile with -fopenmp !!\n");
+#endif
+  }
+
+  exit(EXIT_SUCCESS);
+}
